19e489f282f846650e3ab8f8787a22612063cfb2
kevin quintero reyes <yeykc6@gmail.com>
2014-05-30 09:15:44 -0500

    ultra
    chido

--- js/acciones.js
+++ js/acciones.js
@@ -13,9 +13,10 @@ $(document).ready(function(e) {
  document.addEventListener("deviceready",function(){
   $('#B_Vibrar').tap(function(){
     navigator.notification.vibrate(500);
+	});
 	$('#B_beep').tap(function(){
 	 navigator.notification.beep(1);	
-  });
+  
    });
  },false); //deviceready
 }); //document ready
