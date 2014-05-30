a4da7efcaad3cea1d46b1337f38a3c34e8d10194
kevin quintero reyes <yeykc6@gmail.com>
2014-05-30 09:07:17 -0500

    cambiado
    neww

--- js/acciones.js
+++ js/acciones.js
@@ -13,8 +13,8 @@ $(document).ready(function(e) {
  document.addEventListener("deviceready",function(){
   $('#B_Vibrar').tap(function(){
     navigator.notification.vibrate(500);
-	$('#beep').tap(function(){
-	 navigator.notification.beep(2);	
+	$('#B_beep').tap(function(){
+	 navigator.notification.beep(1);	
   });
    });
  },false); //deviceready
